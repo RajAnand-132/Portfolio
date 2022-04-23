6045a3f7729282fc77980f9994a8d44f5c1f91d7
Raj Anand <83199724+RajAnand-132@users.noreply.github.com>
2022-04-23 16:40:18 +0530

    Add files via upload

---
+++ assets/js/pageredirect.js
@@ -0,0 +1,26 @@
+/**
+ * Statues Bar Control and animation
+ */
+ var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        status.classList.add('success');
+        form.reset();
+
+      }).catch(error => {
+        status.classList.add('error');
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
