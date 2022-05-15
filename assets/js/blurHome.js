74fc7f7ebc8b3d08716b37afd5f0e32bbfd0f3f0
RajAnand-132 <rajanand995500@gmail.com>
2022-05-15 15:11:04 +0530

    blur effect

---
+++ assets/js/blurHome.js
@@ -0,0 +1,13 @@
+const bg = document.querySelector("#hero");
+let load = 0;
+const blurring = () => {
+    load++;
+    if (load >50) clearInterval(int);
+    bg.style.filter = `blur(${scale(load, 0, 50, 40, 0)}px)`;
+};
+
+// For reference: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+let int = setInterval(blurring, 40);
