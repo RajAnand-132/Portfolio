68b1511d6833dac3383c70dea419928572b00379
RajAnand-132 <rajanand995500@gmail.com>
2022-05-17 23:55:20 +0530

    updated

--- assets/js/blurHome.js
+++ assets/js/blurHome.js
@@ -2,8 +2,8 @@ const bg = document.querySelector("#hero");
 let load = 0;
 const blurring = () => {
     load++;
-    if (load >60) clearInterval(int);
-    bg.style.filter = `blur(${scale(load, 0, 60, 40, 0)}px)`;
+    if (load >65) clearInterval(int);
+    bg.style.filter = `blur(${scale(load, 0, 65, 40, 0)}px)`;
 };
 
 // For reference: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
